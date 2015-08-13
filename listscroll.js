265c96512c2d4e683ee026c98266a602c43356aa
prawn <qianbenyingxia@gmail.com>
2015-08-13 10:28:29 +0800

    Update listscroll.js

--- listscroll.js
+++ listscroll.js
@@ -2,7 +2,7 @@
  * @Listscroll   单页应用列表滚动模块
  * @description  适合移动端（滚动条是属于body），保持列表dom个数一定以达到滚动流畅
  * @dependencies 依赖zepto或JQuery，建议使用zepto
- * @email  zhangxia@conew.com
+ * @email  qianbenyingxia@gmail.com
  * @date   2015/08/12
  */
 
